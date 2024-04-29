3ae1be9769770b68bdbd52ffe19879c881a6f3b9
thatimoreira <thatimoreira.dev@gmail.com>
2024-04-29 11:58:43 -0300

    Add Movie class with attributes for movie title, release year, inclusion in plan, rating, total ratings and running time in minutes.

---
+++ src/Movie.java
@@ -0,0 +1,8 @@
+public class Movie {
+    String  movieTitle;             //nomeFilme
+    int     releaseYear;            //anoDeLançamento
+    boolean includedInPlan;         //incluidoNoPlano
+    double  rating;                 //avaliacao
+    int     totalRatinsgs;          //totalDeAvaliacoes
+    int     runningTimesInMinutes;  //duracaoEmMinutos
+}
