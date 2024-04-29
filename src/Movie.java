e2c3af6f6a904270731e31255e05a193aff00779
thatimoreira <thatimoreira.dev@gmail.com>
2024-04-29 14:21:04 -0300

    Refactor Movie class to include a method 'displayMovieInfo()'for showing movie details. this method was added to facilitate the display of movie details in a structured format. It improves code readability and reusability by encapsuling the display logic within the Movie class, enhacing the overall maintainability of the project.

--- src/Movie.java
+++ src/Movie.java
@@ -5,4 +5,18 @@ public class Movie {
     double  rating;                 //avaliacao
     int     totalRatings;          //totalDeAvaliacoes
     int     runningTimesInMinutes;  //duracaoEmMinutos
+
+    void    displayMovieInfo(){
+        System.out.printf("Movie title.....: %s\n", movieTitle);
+        System.out.printf("Release year....: %s\n", releaseYear);
+        if (includedInPlan == true){
+            System.out.println("Included in plan: Yes");
+        }
+        else{
+            System.out.println("Included in plan: No");
+        }
+        System.out.printf("Rating..........: %.2f\n", rating);
+        System.out.printf("Running time....: %d min\n", runningTimesInMinutes);
+        System.out.printf("Total ratings...: %d\n", ++totalRatings);
+    }
 }
