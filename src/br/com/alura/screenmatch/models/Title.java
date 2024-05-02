36d3884704174ea77b25ffe9588862b2e926e978
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-02 17:00:21 -0300

    Create the Title class as the superclass of the refactored class Movie class (previously MovieTitle) and the new Serie class. Both Movie and Serie utilize superclass methods for displayind the title information and calculating ratings. This refactor applies the concept of inheritance, promoting code reuse and enhancing organization.

---
+++ src/br/com/alura/screenmatch/models/Title.java
@@ -0,0 +1,74 @@
+package br.com.alura.screenmatch.models;
+
+public class Title {
+    private String  title;             //nomeFilme
+    private int     releaseYear;            //anoDeLançamento
+    private boolean includedInPlan;         //incluidoNoPlano
+    private double  ratingSum;              //somaDasAvaliacoes
+    private int     totalRatings;           //totalDeAvaliacoes
+    private int     runningTimesInMinutes;  //duracaoEmMinutos
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public boolean isIncludedInPlan() {
+        return includedInPlan;
+    }
+
+    public double getRatingSum() {
+        return ratingSum;
+    }
+
+    public int getRunningTimesInMinutes() {
+        return runningTimesInMinutes;
+    }
+
+    public int getTotalRatings(){
+        return (totalRatings);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public void setIncludedInPlan(boolean includedInPlan) {
+        this.includedInPlan = includedInPlan;
+    }
+
+    public void setRunningTimesInMinutes(int runningTimesInMinutes) {
+        this.runningTimesInMinutes = runningTimesInMinutes;
+    }
+
+    public void displayTitleInfo(){
+        System.out.printf("Title..............: %s\n", title);
+        System.out.printf("Release year.......: %d\n", releaseYear);
+        if (includedInPlan == true){
+            System.out.println("Included in plan...: Yes");
+        }
+        else{
+            System.out.println("Included in plan...: No");
+        }
+    }
+
+    public void    updateRatingSum(double newRating){
+        if (newRating >= 0 && newRating <= 10){
+            ratingSum += newRating;
+            totalRatings++;
+        } else{
+            System.out.println("Invalid rating. the rating must be equal or greater than zero");
+        }
+    }
+
+    public double  calculateRatingAverage(){
+        return (ratingSum/totalRatings);
+    }
+}
