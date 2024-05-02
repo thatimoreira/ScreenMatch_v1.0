5ea7638dc968e4bb9a16b725cff750748868b71b
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-02 17:27:14 -0300

    Add Serie class, inheriting from the Title superclass. Add fields for seasons, episodes per season, and minutes per episode. The 'active' field indicates if new episodes will be released. Override the getRunningTimeInMinutes() method to calculate the total running time based on the number of seasons, episodes per season, and minutes per episode. This addition improves code organization and leverages inheritance for code reuse.

--- src/br/com/alura/screenmatch/models/Serie.java
+++ src/br/com/alura/screenmatch/models/Serie.java
@@ -1,2 +1,45 @@
-package br.com.alura.screenmatch.models;public class Serie {
+package br.com.alura.screenmatch.models;
+
+public class Serie extends Title {
+    private int seasons;
+    private int episodesPerSeason;
+    private boolean active;         //indicates if new episodes will be released
+    private int minutesPerEpisode;
+
+    public int getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(int seasons) {
+        this.seasons = seasons;
+    }
+
+    public int getEpisodesPerSeason() {
+        return episodesPerSeason;
+    }
+
+    public void setEpisodesPerSeason(int episodesPerSeason) {
+        this.episodesPerSeason = episodesPerSeason;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getMinutesPerEpisode() {
+        return minutesPerEpisode;
+    }
+
+    public void setMinutesPerEpisode(int minutesPerEpisode) {
+        this.minutesPerEpisode = minutesPerEpisode;
+    }
+
+    @Override
+    public int getRunningTimeInMinutes() {
+        return seasons * episodesPerSeason * minutesPerEpisode;
+    }
 }
