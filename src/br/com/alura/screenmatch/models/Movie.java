a632b6cab3d98cd2233b5e999424d7f45c90138b
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-02 17:07:29 -0300

    Refactor Movie class to inherit from the Title superclass. Remove redundant fields and methods related to title information and ratings, as they are now inherited from the title superclass. Add a new field 'director' to Movie class to specialize its fields. This refactor aims to apply the concept of inheritance, promoting code reuse and better organization.

--- src/br/com/alura/screenmatch/models/Movie.java
+++ src/br/com/alura/screenmatch/models/Movie.java
@@ -1,75 +1,13 @@
 package br.com.alura.screenmatch.models;
 
-public class Movie {
-    private String   movieTitle;             //nomeFilme
-    private int      releaseYear;            //anoDeLançamento
-    private boolean  includedInPlan;         //incluidoNoPlano
-    private double  ratingSum;              //somaDasAvaliacoes
-    private int     totalRatings;           //totalDeAvaliacoes
-    private int      runningTimesInMinutes;  //duracaoEmMinutos
+public class Movie extends Title {
+    private String  director;
 
-    public String getMovieTitle() {
-        return movieTitle;
+    public String getDirector() {
+        return director;
     }
 
-    public int getReleaseYear() {
-        return releaseYear;
-    }
-
-    public boolean isIncludedInPlan() {
-        return includedInPlan;
-    }
-
-    public double getRatingSum() {
-        return ratingSum;
-    }
-
-    public int getRunningTimesInMinutes() {
-        return runningTimesInMinutes;
-    }
-
-    public int getTotalRatings(){
-        return (totalRatings);
-    }
-
-    public void setMovieTitle(String movieTitle) {
-        this.movieTitle = movieTitle;
-    }
-
-    public void setReleaseYear(int releaseYear) {
-        this.releaseYear = releaseYear;
-    }
-
-    public void setIncludedInPlan(boolean includedInPlan) {
-        this.includedInPlan = includedInPlan;
-    }
-
-    public void setRunningTimesInMinutes(int runningTimesInMinutes) {
-        this.runningTimesInMinutes = runningTimesInMinutes;
-    }
-
-    public void    displayMovieInfo(){
-        System.out.printf("br.com.alura.screenmatch.models.Movie title.....: %s\n", movieTitle);
-        System.out.printf("Release year....: %d\n", releaseYear);
-        if (includedInPlan == true){
-            System.out.println("Included in plan: Yes");
-        }
-        else{
-            System.out.println("Included in plan: No");
-        }
-        System.out.printf("Running time....: %d min\n", runningTimesInMinutes);
-    }
-
-    public void    updateRatingSum(double newRating){
-        if (newRating >= 0 && newRating <= 10){
-            ratingSum += newRating;
-            totalRatings++;
-        } else{
-            System.out.println("Invalid rating. the rating must be equal or greater than zero");
-        }   
-    }
-
-    public double  calculateRatingAverage(){
-        return (ratingSum/totalRatings);
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
