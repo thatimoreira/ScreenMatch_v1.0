acab1b8483666ab87c451f1b4a8d464153772a4d
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-02 17:45:12 -0300

    Update Main class to include Serie instance. Also, update method calls to match new mwthod names for calculating and displaying information. This refactor enhances the Main class to handle both movies and series, improving code readability and maintainability.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,37 @@
 import br.com.alura.screenmatch.models.Movie;
+import br.com.alura.screenmatch.models.Serie;
 
 public class Main {
     public static void main(String[] args) {
-        Movie movie1 = new Movie();
-        movie1.setMovieTitle("The Godfather");
-        movie1.setReleaseYear(1972);
-        movie1.setIncludedInPlan(true);
-        movie1.setRunningTimesInMinutes(175);
+        Movie godfather = new Movie();
+        godfather.setTitle("The Godfather");
+        godfather.setReleaseYear(1972);
+        godfather.setIncludedInPlan(true);
+        godfather.setRunningTimesInMinutes(175);
 
-        movie1.displayMovieInfo();
-        movie1.updateRatingSum(8);
-        movie1.updateRatingSum(7.5);
-        movie1.updateRatingSum(9.8);
-        System.out.printf("Total ratings...: %d\n", movie1.getTotalRatings());
-        System.out.printf("Rating average..: %.2f\n", movie1.calculateRatingAverage());
+        System.out.println("\n------------------------- MOVIE -------------------------");
+        godfather.displayTitleInfo();
+        godfather.updateRatingSum(8);
+        godfather.updateRatingSum(7.5);
+        godfather.updateRatingSum(9.8);
+        System.out.printf("Running time.......: %d min\n", godfather.getRunningTimeInMinutes());
+        System.out.printf("Total ratings......: %d\n", godfather.getTotalRatings());
+        System.out.printf("Rating average.....: %.2f\n", godfather.calculateRatingAverage());
+
+        System.out.println("\n------------------------- SERIE -------------------------");
+        Serie   demonSlayer = new Serie();
+        demonSlayer.setTitle("Demon Slayer - Kimetsu no Yaiba");
+        demonSlayer.setReleaseYear(2016);
+        demonSlayer.setIncludedInPlan(true);
+        demonSlayer.updateRatingSum(7.5);
+        demonSlayer.updateRatingSum(8.1);
+        demonSlayer.updateRatingSum(7);
+        demonSlayer.setEpisodesPerSeason(18);
+        demonSlayer.setMinutesPerEpisode(25);
+        demonSlayer.setSeasons(4);
+        demonSlayer.displayTitleInfo();
+        System.out.printf("Time to binge-watch: %dmin\n", demonSlayer.getRunningTimeInMinutes());
+        System.out.printf("Total ratings......: %d\n", demonSlayer.getTotalRatings());
+        System.out.printf("Rating average.....: %.2f\n", demonSlayer.calculateRatingAverage());
     }
 }
