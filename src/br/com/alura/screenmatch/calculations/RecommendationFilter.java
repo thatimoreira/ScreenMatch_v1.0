2622dbd79aefa656b9c1fbd3723ef14c8c2985cb
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-06 13:25:25 -0300

    Implement Episode class. Add Episode class with fields for episode number, name, associated series, and total views. Implement Rateable interface to calculate the rating based on the total views.

--- src/br/com/alura/screenmatch/calculations/RecommendationFilter.java
+++ src/br/com/alura/screenmatch/calculations/RecommendationFilter.java
@@ -1,2 +1,13 @@
-package br.com.alura.screenmatch.calculations;public class RecommendationFilter {
+package br.com.alura.screenmatch.calculations;
+
+public class RecommendationFilter {
+    public void filter(Rateable rateable){
+        if (rateable.getRating() >= 4){
+            System.out.println("Top watches!");
+        } else if (rateable.getRating() >= 2){
+            System.out.println("Popular watches!");
+        } else {
+            System.out.println("Add to Watchlist");
+        }
+    }
 }
--- src/br/com/alura/screenmatch/models/Episode.java
+++ src/br/com/alura/screenmatch/models/Episode.java
@@ -1,2 +1,59 @@
-package br.com.alura.screenmatch.models;public class Episode {
+package br.com.alura.screenmatch.models;
+
+import br.com.alura.screenmatch.calculations.Rateable;
+
+public class Episode extends Serie implements Rateable {
+    private int     number;
+    private String  name;
+    private Serie   serie;
+    private int     totalViews;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+
+    public int getTotalViews() {
+        return totalViews;
+    }
+
+    public void setTotalViews(int totalViews) {
+        this.totalViews = totalViews;
+    }
+
+    @Override
+    public int getRating() {
+        if (totalViews == 0){
+            return 0;
+        } else if (totalViews <= 100) {
+            return 1;
+        } else if (totalViews <= 300) {
+            return 2;
+        } else if (totalViews <= 600) {
+            return 3;
+        } else if (totalViews < 1000) {
+            return 4;
+        } else {
+            return 5;
+        }
+    }
 }
