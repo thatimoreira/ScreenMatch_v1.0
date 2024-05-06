9bc76c98f3bcc92229171ff54cbe06069d6aad45
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-06 13:29:59 -0300

    Refactor Main class to include instances of Movie and Episode, demonstrating the use of inheritance and polymorphism from the Title, Movie, and Episode classes. This refactor emphasizes the code's flexibility and readability, showcasing the fundamental concepts of object-oriented programming.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import br.com.alura.screenmatch.calculations.Rateable;
+import br.com.alura.screenmatch.calculations.RecommendationFilter;
 import br.com.alura.screenmatch.calculations.TimeCalculator;
+import br.com.alura.screenmatch.models.Episode;
 import br.com.alura.screenmatch.models.Movie;
 import br.com.alura.screenmatch.models.Serie;
 
@@ -10,7 +13,7 @@ public class Main {
         godfather.setIncludedInPlan(true);
         godfather.setRunningTimesInMinutes(175);
 
-        System.out.println("\n------------------------- MOVIE -------------------------");
+        System.out.println("\n---------------------------- MOVIE ----------------------------");
         godfather.displayTitleInfo();
         godfather.updateRatingSum(8);
         godfather.updateRatingSum(7.5);
@@ -19,8 +22,8 @@ public class Main {
         System.out.printf("Total ratings......: %d\n", godfather.getTotalRatings());
         System.out.printf("Rating average.....: %.2f\n", godfather.calculateRatingAverage());
 
-        System.out.println("\n------------------------- SERIE -------------------------");
-        Serie   demonSlayer = new Serie();
+        System.out.println("\n---------------------------- SERIE ----------------------------");
+        Episode demonSlayer = new Episode();
         demonSlayer.setTitle("Demon Slayer - Kimetsu no Yaiba");
         demonSlayer.setReleaseYear(2016);
         demonSlayer.setIncludedInPlan(true);
@@ -35,10 +38,35 @@ public class Main {
         System.out.printf("Total ratings......: %d\n", demonSlayer.getTotalRatings());
         System.out.printf("Rating average.....: %.2f\n", demonSlayer.calculateRatingAverage());
 
-        System.out.println("\n-------------------- BINGE-WATCH TIME --------------------");
+        System.out.println("\n---------------------- BINGE-WATCH TIME ----------------------");
         TimeCalculator  calculator = new TimeCalculator();
         calculator.add(godfather);
         calculator.add(demonSlayer);
+        System.out.printf("Titles: '%s' and '%s'\n", godfather.getTitle(), demonSlayer.getTitle());
         System.out.printf("Binge-watch time: %s min\n", calculator.getTotalTime());
+
+        System.out.println("\n----------------------- RECOMMENDATION -----------------------");
+        RecommendationFilter    recommendationFilter = new RecommendationFilter();
+        System.out.printf("Serie....: %s\n", demonSlayer.getTitle());
+        demonSlayer.setName("Cruelty");
+        demonSlayer.setNumber(1);
+        demonSlayer.setTotalViews(1500);
+        System.out.printf("Episode %d: '%s' -> ", demonSlayer.getNumber(), demonSlayer.getName());
+        recommendationFilter.filter(demonSlayer);
+        demonSlayer.setName("Trainer Sakonji Urokodaki");
+        demonSlayer.setNumber(2);
+        demonSlayer.setTotalViews(1200);
+        System.out.printf("Episode %d: '%s' -> ", demonSlayer.getNumber(), demonSlayer.getName());
+        recommendationFilter.filter(demonSlayer);
+        demonSlayer.setName("Sabito and Makomo");
+        demonSlayer.setNumber(3);
+        demonSlayer.setTotalViews(900);
+        System.out.printf("Episode %d: '%s' -> ", demonSlayer.getNumber(), demonSlayer.getName());
+        recommendationFilter.filter(demonSlayer);
+        demonSlayer.setName("Final selection");
+        demonSlayer.setNumber(4);
+        demonSlayer.setTotalViews(600);
+        System.out.printf("Episode %d: '%s' -> ", demonSlayer.getNumber(), demonSlayer.getName());
+        recommendationFilter.filter(demonSlayer);
     }
 }
