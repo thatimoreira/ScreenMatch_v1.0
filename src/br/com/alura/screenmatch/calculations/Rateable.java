90867fabbe3fa20c34270258178f0fd6156973ef
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-06 13:22:18 -0300

    Implement Rateable interface. Created Rateable interface with method getRating() to define a contract for classes that can be rated.

---
+++ src/br/com/alura/screenmatch/calculations/Rateable.java
@@ -0,0 +1,5 @@
+package br.com.alura.screenmatch.calculations;
+
+public interface Rateable {
+    int getRating();
+}
---
+++ src/br/com/alura/screenmatch/calculations/RecommendationFilter.java
@@ -0,0 +1,2 @@
+package br.com.alura.screenmatch.calculations;public class RecommendationFilter {
+}
