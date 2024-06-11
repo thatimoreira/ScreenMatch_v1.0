c1904bd3e4c7b30c58f1bf568520b0175fd36438
thatimoreira <thatimoreira.dev@gmail.com>
2024-06-11 05:18:53 -0300

    Implements the Movie constructor.

--- src/br/com/alura/screenmatch/models/Movie.java
+++ src/br/com/alura/screenmatch/models/Movie.java
@@ -5,10 +5,16 @@ import br.com.alura.screenmatch.calculations.Rateable;
 public class Movie extends Title implements Rateable {
     private String  director;
 
+    // Constructor -> same name as the class and returns nothing
+    public Movie(String title) {
+        this.setTitle(title);
+    }
+
     public String getDirector() {
         return director;
     }
 
+    // Now a constructor was created (line 9), so is this setter really necessary?
     public void setDirector(String director) {
         this.director = director;
     }
