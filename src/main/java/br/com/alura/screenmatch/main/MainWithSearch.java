879dc434ab9fa90b62811f40b9e5c7ca7656f371
thatimoreira <thatimoreira.dev@gmail.com>
2024-07-12 01:19:02 -0300

    Refactor MainWithSearch: update package structure, add Gson parsing
    - Added Gson dependency to parse JSON response from OMDB API
    - Introduced new Title model to represent parsed movie data

---
+++ src/main/java/br/com/alura/screenmatch/main/MainWithSearch.java
@@ -0,0 +1,38 @@
+package br.com.alura.screenmatch.main;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+
+import br.com.alura.screenmatch.models.Title;
+
+public class MainWithSearch {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Qual filme deseja buscar? ");
+        var search = keyboard.nextLine();
+
+        String adress = "https://www.omdbapi.com/?t=" + search + "&apikey=7bc2b861";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+         .uri(URI.create(adress))
+         .build();
+    HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+    
+    String json = response.body();
+    System.out.println(json);
+
+    Gson gson = new Gson();
+    Title myTile = gson.fromJson(json, Title.class);
+    System.out.println("Title: " + myTile.getTitle());
+
+    keyboard.close();
+    }
+}
