ef02ef8c6ae6169a7d50a3dc14795b26f983cd56
thatimoreira <thatimoreira.dev@gmail.com>
2024-07-12 04:08:29 -0300

    Add exception handling to MainWithSearch
    - Added try-catch block to handle potential NumberFormatException and IllegalArgumentException
    - Ensured proper error messages are displayed for each exception
    - Added final success message for clarity

--- src/main/java/br/com/alura/screenmatch/main/MainWithSearch.java
+++ src/main/java/br/com/alura/screenmatch/main/MainWithSearch.java
@@ -23,25 +23,34 @@ public class MainWithSearch {
 
         String adress = "https://www.omdbapi.com/?t=" + search + "&apikey=7bc2b861";
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-         .uri(URI.create(adress))
-         .build();
-    HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-    
-    String json = response.body();
-    System.out.println(json);
+        try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(adress))
+                                .build();
+                HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+                
+                String json = response.body();
+                System.out.println(json);
 
-    Gson gson = new GsonBuilder()
-            .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-            .create();
-    OmdbTitle myOmdbTitle = gson.fromJson(json, OmdbTitle.class);
-    System.out.println(myOmdbTitle);
+                Gson gson = new GsonBuilder()
+                        .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                        .create();
+                OmdbTitle myOmdbTitle = gson.fromJson(json, OmdbTitle.class);
+                System.out.println(myOmdbTitle);
 
-    Title myTitle = new Title(myOmdbTitle);
-    System.out.println("Converted title:");
-    System.out.println(myTitle);
+                    Title myTitle = new Title(myOmdbTitle);
+                    System.out.println("Converted title:");
+                    System.out.println(myTitle);
+        } catch (NumberFormatException e) {
+            System.out.print("Error: ");
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: please check the argument(s)");
+        }
 
-    keyboard.close();
+        System.out.println("EXIT SUCCESS!");
+
+        keyboard.close();
     }
 }
