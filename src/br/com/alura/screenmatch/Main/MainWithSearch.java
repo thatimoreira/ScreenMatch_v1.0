55ee1e62692ed65672a3675d6387a3eb99412e99
thatimoreira <thatimoreira.dev@gmail.com>
2024-07-11 22:55:23 -0300

    Add search by film title.

--- src/br/com/alura/screenmatch/Main/MainWithSearch.java
+++ src/br/com/alura/screenmatch/Main/MainWithSearch.java
@@ -6,14 +6,23 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
 
 public class MainWithSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Qual filme deseja buscar? ");
+        var search = keyboard.nextLine();
+
+        String adress = "https://www.omdbapi.com/?t=" + search + "&apikey=7bc2b861";
+
         HttpClient client = HttpClient.newHttpClient();
    HttpRequest request = HttpRequest.newBuilder()
-         .uri(URI.create("https://www.omdbapi.com/?i=tt3896198&apikey=7bc2b861"))
+         .uri(URI.create(adress))
          .build();
     HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
     System.out.println(response.body());
+
+    keyboard.close();
     }
 }
